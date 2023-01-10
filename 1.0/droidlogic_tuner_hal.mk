c496c1397d1f8375840e345e7cf3d601cd90bbf0
junchao yuan <junchao.yuan@amlogic.com>
2023-01-10 17:18:42 +0800

    tunerhal: config tunerhal vts [1/1]
    PD#SWPL-106270
    
    Problem:
    no tunerhal vts config
    
    Solution:
    config tunerhal vts
    
    Verify:
    ohm
    
    Change-Id: I6569b1558bd9b2cd1dbff9305e4cdc3d2571c2d0
    Signed-off-by: junchao yuan <junchao.yuan@amlogic.com>

--- 1.0/droidlogic_tuner_hal.mk
+++ 1.0/droidlogic_tuner_hal.mk
@@ -4,11 +4,11 @@ USE_TH_TEST_SETUP := true
 #$(warning TARGET_PRODUCT=$(TARGET_PRODUCT))
 
 PRODUCT_PACKAGES += \
-    android.hardware.tv.tuner@1.0-service.droidlogic \
-    tuner_vts_config_1_0.xml
+    android.hardware.tv.tuner@1.0-service.droidlogic
 
 PRODUCT_COPY_FILES += \
-    frameworks/native/data/etc/android.hardware.tv.tuner.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.hardware.tv.tuner.xml
+    frameworks/native/data/etc/android.hardware.tv.tuner.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.hardware.tv.tuner.xml \
+    hardware/amlogic/tuner/config/tuner_vts_config_1_0.xml:$(TARGET_COPY_OUT_VENDOR)/etc/tuner_vts_config_1_0.xml
 
 ifeq ($(USE_TH_TEST_SETUP), true)
 PRODUCT_PACKAGES += \
--- aidl/droidlogic_tuner_hal.mk
+++ aidl/droidlogic_tuner_hal.mk
@@ -7,7 +7,8 @@ PRODUCT_PACKAGES += \
     android.hardware.tv.tuner-service.droidlogic
 
 PRODUCT_COPY_FILES += \
-    frameworks/native/data/etc/android.hardware.tv.tuner.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.hardware.tv.tuner.xml
+    frameworks/native/data/etc/android.hardware.tv.tuner.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.hardware.tv.tuner.xml \
+    hardware/amlogic/tuner/config/tuner_vts_config_aidl_V1.xml:$(TARGET_COPY_OUT_VENDOR)/etc/tuner_vts_config_aidl_V1.xml
 
 ifeq ($(USE_TH_TEST_SETUP), true)
 PRODUCT_PACKAGES += \
