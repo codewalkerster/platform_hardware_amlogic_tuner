3d2998399690a02d6375019f252f5b539079e2f8
junchao yuan <junchao.yuan@amlogic.com>
2023-02-21 14:54:56 +0800

    tunerhal: fix TunerFrameworkSetup compile error in R [1/1]
    PD#SWPL-112012
    
    Problem:
    TunerFrameworkSetup compile error in R when enable tunerhal1.1
    
    Solution:
    don't compile TunerFrameworkSetup apk;
    
    Verify:
    ohm
    
    Change-Id: I3e476f2672e832f15a0336706b0c0d644cc1c5a6
    Signed-off-by: junchao yuan <junchao.yuan@amlogic.com>

--- 1.0/droidlogic_tuner_hal.mk
+++ 1.0/droidlogic_tuner_hal.mk
@@ -10,7 +10,3 @@ PRODUCT_COPY_FILES += \
     frameworks/native/data/etc/android.hardware.tv.tuner.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.hardware.tv.tuner.xml \
     hardware/amlogic/tuner/config/tuner_vts_config_1_0.xml:$(TARGET_COPY_OUT_VENDOR)/etc/tuner_vts_config_1_0.xml
 
-ifeq ($(USE_TH_TEST_SETUP), true)
-PRODUCT_PACKAGES += \
-    TunerFrameworkSetup
-endif
