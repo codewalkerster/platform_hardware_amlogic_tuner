3bca2fd0d5453d072f1866c8ec1d60b2bde28774
wenjie.ren <wenjie.ren@amlogic.com>
2024-05-24 16:05:10 +0800

    tunerhal: fix vts issue [1/1]
    PD#SWPL-169559
    
    Problem:
    can't read vts config file
    
    Solution:
    need modify vts config file's name
    
    Verify:
    android u
    
    Change-Id: I4921959f5cb182fbf8c30262135e0e6896d91eca
    Signed-off-by: wenjie ren <wenjie.ren@amlogic.com>

--- aidl/droidlogic_tuner_hal.mk
+++ aidl/droidlogic_tuner_hal.mk
@@ -2,13 +2,14 @@
 USE_TH_TEST_SETUP := false
 #$(warning USE TEST TESTUP=$(USE_TH_TEST_SETUP))
 #$(warning TARGET_PRODUCT=$(TARGET_PRODUCT))
+PRODUCT_VENDOR_PROPERTIES += ro.vendor.vts_tuner_configuration_variant=amlogic
 
 PRODUCT_PACKAGES += \
     android.hardware.tv.tuner-service.droidlogic
 
 PRODUCT_COPY_FILES += \
     frameworks/native/data/etc/android.hardware.tv.tuner.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.hardware.tv.tuner.xml \
-    hardware/amlogic/tuner/config/tuner_vts_config_aidl_V1.xml:$(TARGET_COPY_OUT_VENDOR)/etc/tuner_vts_config_aidl_V1.xml
+    hardware/amlogic/tuner/config/tuner_vts_config_aidl_V1.xml:$(TARGET_COPY_OUT_VENDOR)/etc/tuner_vts_config_aidl_V1.amlogic.xml
 
 ifeq ($(USE_TH_TEST_SETUP), true)
 PRODUCT_PACKAGES += \
--- aidl/droidlogic_tuner_hal_lazy.mk
+++ aidl/droidlogic_tuner_hal_lazy.mk
@@ -3,13 +3,15 @@ USE_TH_TEST_SETUP := false
 #$(warning USE TEST TESTUP=$(USE_TH_TEST_SETUP))
 #$(warning TARGET_PRODUCT=$(TARGET_PRODUCT))
 
-PRODUCT_VENDOR_PROPERTIES += ro.tuner.lazyhal=true
+PRODUCT_VENDOR_PROPERTIES += ro.tuner.lazyhal=true \
+    ro.vendor.vts_tuner_configuration_variant=amlogic
+
 PRODUCT_PACKAGES += \
     android.hardware.tv.tuner-service.droidlogic-lazy
 
 PRODUCT_COPY_FILES += \
     frameworks/native/data/etc/android.hardware.tv.tuner.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.hardware.tv.tuner.xml \
-    hardware/amlogic/tuner/config/tuner_vts_config_aidl_V1.xml:$(TARGET_COPY_OUT_VENDOR)/etc/tuner_vts_config_aidl_V1.xml
+    hardware/amlogic/tuner/config/tuner_vts_config_aidl_V1.xml:$(TARGET_COPY_OUT_VENDOR)/etc/tuner_vts_config_aidl_V1.amlogic.xml
 
 ifeq ($(USE_TH_TEST_SETUP), true)
 PRODUCT_PACKAGES += \
