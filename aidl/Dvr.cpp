c4483a1dc9cbb74701cbc929832ade98a43f8f5e
junchao yuan <junchao.yuan@amlogic.com>
2024-11-29 21:39:01 +0800

    tunerhal: exit dvr thread in dvr close interface [1/1]
    PD#SWPL-196406
    
    Problem:
    tunerhal crashed, because playback and record thread don't be exited
    
    Solution:
    exit playback and record thread in dvr close interface
    
    Verify:
    T5M
    
    Change-Id: I12b4060a7c2f45d4fdd9a502eacadacee363c649
    Signed-off-by: junchao yuan <junchao.yuan@amlogic.com>

--- aidl/Dvr.cpp
+++ aidl/Dvr.cpp
@@ -269,6 +269,14 @@ Dvr::~Dvr() {
         if (ret != DVR_SUCCESS) {
             ALOGD("close dvr playback failed!\n");
         }
+        //avoid tunerhal crash, because app didn't call DvrPlayback stop interface to exit playback thread;
+        if (mDvrThreadRunning) {
+            mDvrThreadRunning = false;
+            if (mDvrThread.joinable()) {
+                mDvrEventFlag->wake(static_cast<uint32_t>(DemuxQueueNotifyBits::DATA_READY));
+                mDvrThread.join();
+            }
+        }
         mDemux->setPlaybackHandle(NULL);
     } else if (mType == DvrType::RECORD) {
         if (mOpenParams.dmx_dev_id[1] != 0 && mOpenParams.dmx_dev_id[2] != 0) {
@@ -280,6 +288,18 @@ Dvr::~Dvr() {
         DVR_Result_t ret = dvr_record_close(mRecordhandle);
         if (ret != DVR_SUCCESS) {
             ALOGD("close dvr record failed!\n");
+        }
+        //avoid tunerhal crash, because app didn't call DvrRecord stop interface to exit record thread;
+        if (mDvrRecordThreadRunning) {
+            mDvrRecordThreadRunning = false;
+            if (mDvrRecordThread.joinable()) {
+                mDvrRecordThread.join();
+            }
+
+            if (mReceiveParams.buf) {
+                free(mReceiveParams.buf);
+                mReceiveParams.buf = NULL;
+            }
         }
         mDemux->setRecordHandle(NULL);
     }
